a8f0ac0923eed09a45cce650d1bb109adf2aafd4
Shubham Parihar <shubhamparihar391@gmail.com>
2024-10-18 13:30:39 +0530

    feat: add server side caching for /api/artist route

--- src/app/api/artists/route.ts
+++ src/app/api/artists/route.ts
@@ -1,6 +1,9 @@
 import { getAccessToken } from '@/util/utils';
 import type { ArtistObject, GetSearchResponse } from 'spotify-api-types';
 
+// One week in seconds (60 * 60 * 24 * 7 = 604800 seconds)
+const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
+
 /**
  * GET `/api/artists`
  */
@@ -13,6 +16,9 @@ export async function GET(request: Request) {
 	const url = encodeURI(`https://api.spotify.com/v1/search?q=${query}&type=artist&limit=10`);
 	const accessToken = await getAccessToken();
 	const res = await fetch(url, {
+		next: {
+			revalidate: ONE_WEEK_IN_SECONDS,
+		},
 		headers: { Authorization: `Bearer ${accessToken}` },
 	});
 	const data: GetSearchResponse = await res.json();
