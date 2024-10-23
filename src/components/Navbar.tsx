5ecd754416e546566918a681a38808e2a674e398
Shubham Parihar <shubhamparihar391@gmail.com>
2024-10-24 03:26:23 +0530

    feat: add github and twitter link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,29 @@
+import { BsGithub, BsTwitter } from 'react-icons/bs';
+
 export function Navbar() {
 	return (
-		<nav className='flex flex-row justify-start bg-[#1ED760] px-6'>
-			<h1 className='py-2 text-2xl uppercase text-[#121212]'>Orpheus</h1>
+		<nav className='flex flex-row items-center justify-between bg-[#1ED760] px-6 py-2'>
+			<h1 className='text-2xl uppercase text-[#121212]'>Orpheus</h1>
+			<div className='flex flex-row gap-x-5'>
+				<a
+					href='https://github.com/iShibi'
+					title='github.com/iShibi'
+					target='_blank'
+					rel='noopener noreferrer'
+					className=''
+				>
+					<BsGithub className='h-8 w-8' />
+				</a>
+				<a
+					href='https://twitter.com/iShiibi'
+					title='twitter.com/iShiibi'
+					target='_blank'
+					rel='noopener noreferrer'
+					className=''
+				>
+					<BsTwitter className='h-8 w-8' />
+				</a>
+			</div>
 		</nav>
 	);
 }
