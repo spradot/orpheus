df3c9486f882e2ca639ddf6eb8cf97ad30b94f05
Shubham Parihar <shubhamparihar391@gmail.com>
2024-10-27 20:43:23 +0530

    feat: add theme-color tag and set twitter card to summary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import { Dialog } from '@/components/Dialog';
 import { Navbar } from '@/components/Navbar';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
 	title: 'Orpheus',
 	description: 'Get music recommendations based on artists, tracks, genres, and track attributes',
+	twitter: {
+		card: 'summary',
+	},
+};
+
+export const viewport: Viewport = {
+	themeColor: '#1ED760',
 };
 
 export default function RootLayout({
